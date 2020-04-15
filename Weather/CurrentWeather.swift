312959f1d977b7a4032f1e0b3ffef3ddb8a6030b
Arthur <takerarthur@gmail.com>
2020-04-15 14:43:59 +0300

    Weather Icon Manager

---
+++ Weather/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  Weather
+//
+//  Created by Arthur Batyaev on 4/15/20.
+//  Copyright © 2020 Arthur Batyaev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+    let temperature: Double
+    let appearebtTemperature: Double
+    let humidity: Double
+    let pressure: Double
+    let icon: UIImage
+}
--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//MARK: - IBOutlets
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var temperetureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
